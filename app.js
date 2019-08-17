9280d704fe265ea00e41390de0f373c99f3cc940
behzadkk <bhzdkkvnd@gmail.com>
2019-08-17 17:40:53 +0100

    REST Index

---
+++ app.js
@@ -0,0 +1,46 @@
+let express = require("express"),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  app = express();
+
+let PORT = process.env.PORT || 3000;
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+// Blog.create({
+//   title: "Test Blog",
+//   image: "https://unsplash.com/photos/s7qZayMy6Go",
+//   body: "HELLO THIS IS A BLOG POST"
+// });
+
+// RESTFUL ROUTES
+app.get("/", function(req, res) {
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log("ERROR!");
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+app.listen(PORT, function() {
+  console.log("SERVER IS RUNNING AT PORT " + PORT);
+});
